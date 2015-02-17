679052b0aac429d5dbdd16708b91a7ec5171da5b
Chasil <l0k0@interia.pl>
2015-02-17 09:25:10 +0100

    zmiana jakaśtam

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -17,7 +17,7 @@
 							</ul>
 						</div>
 					@endif
-
+ 
 					<form class="form-horizontal" role="form" method="POST" action="/auth/login">
 						<input type="hidden" name="_token" value="{{ csrf_token() }}">
 
