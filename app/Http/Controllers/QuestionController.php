2615aae5878dfb8fa3a3d8e00ca058150d76fca1
Tuminure <tuminure@gmail.com>
2015-02-26 15:33:11 +0100

    question fixes

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -129,69 +129,56 @@ class QuestionController extends Controller {
      */
     public function uploadQuestion()
     {
-        $question = $_POST['question'];
-        $answer_a = $_POST['answer-1'];
-        $answer_b = $_POST['answer-2'];
-        $answer_c = $_POST['answer-3'];
-        $correct_answer = $_POST['correct_answer'];
-        $category = $_POST['category'];
-        $accepted = '0';
-        
-        $query = new \App\QuestionsAbc();
-        
-        if($_FILES['uploadedfile']['size'] > 0)
-        {
-            $extension = substr($_FILES['uploadedfile']['name'], strrpos($_FILES['uploadedfile']['name'], '.') +1);
+        if(Request::isMethod('post')) {
+            
+            $data = [
+                'picture' => Request::file('uploaded_picture'),
+                'question' => Input::get('question'),
+                'answer_a' => Input::get('answer-1'),
+                'answer_b' => Input::get('answer-2'),
+                'answer_c' => Input::get('answer-3'),
+                'correct_answer' => Input::get('correct_answer'),
+                'accepted' => false,
+                'category' => Input::get('category')
+            ];
 
-            do {
-                $randomCode = substr(str_shuffle("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"), 0, 1).substr(md5(time()),1,8);
-                $count = \App\QuestionsAbc::where('picture', '=', $randomCode)->count();
-            }
-            while($count > 1);
-            
-            $final_name = $randomCode . '.' . $extension;
-            
-            
-            $target = __DIR__ . '/../../../public_html/images/';
-            $target_final = $target . basename($final_name);
+            $validator = Validator::make($data, QuestionsAbc::getValidationAddRules());
+            $errorMessages = $validator->messages();
 
-            if(move_uploaded_file($_FILES['uploadedfile']['tmp_name'], $target_final)) {
-                
-                $query->question = $question;
-                $query->answer_a = $answer_a;
-                $query->answer_b = $answer_b;
-                $query->answer_c = $answer_c;
-                $query->accepted = $accepted;
-                $query->correct_answer = $correct_answer;
-                $query->picture = $final_name;
-                $query->category = $category;
-            
-                if($query->save()) {
-    //                echo json_encode();
-                } else {
-    //                echo json_encode();
+            if(Request::hasFile('uploaded_picture') && $validator->passes()) {
+                $extension = Request::file('uploaded_picture')->getClientOriginalExtension();
+
+                $picture = null;
+
+                while(!$picture) {
+                    $random_name = str_random(12) . $extension;
+                    if(!QuestionsAbc::where('picture', '=', $random_name)->first()) {
+                        $picture = $random_name;
+                    }
+                }
+
+                $data['picture'] = $picture;
+
+                try {
+                    Request::file('uploaded_picture')->move(QuestionsAbc::getImagePath(), $picture);
+                } catch (Exception $e) {
+                    $errorMessages->add('picture', 'An error message.');
                 }
             }
-        }
-        else {
-            
-            $query->question = $question;
-            $query->answer_a = $answer_a;
-            $query->answer_b = $answer_b;
-            $query->answer_c = $answer_c;
-            $query->accepted = $accepted;
-            $query->correct_answer = $correct_answer;
-            $query->picture = '';
-            $query->category = $category;
-            if($query->save()) {
-//                echo json_encode();
-            }
-            else {
-//                echo json_encode();
-            }
-        }
 
-        return view('addAbcQuestion');
+            if($errorMessages->isEmpty()) {
+                $questionRow = new QuestionsAbc();
+
+                if(!$questionRow->fill($data)->save()) {
+                    $errorMessages->add('form', 'An error message.');
+                }
+            }
+
+            return view('addAbcQuestion', [
+                'messages' => $errorMessages
+            ]);
+        }
+        
     }
 
     
