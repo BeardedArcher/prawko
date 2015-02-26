cc1dd71f709d478c41a59b76a73dc3e8cdbf8dc6
Chasil <l0k0@interia.pl>
2015-02-26 15:52:02 +0100

    first part of main page

---
+++ resources/views/navbar.php
@@ -0,0 +1,24 @@
+<nav class="navbar navbar-default">
+    <div class="container-fluid">
+            <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                            <span class="sr-only">Toggle Navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#">Główna</a>
+            </div>
+
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav navbar-nav">
+                            <li><a href="/admin/logowanie">Logowanie</a></li>
+                            <li><a href="pytanie">Odpowiedz na pytanie</a></li>
+                            <li><a href="pytania-abc">Lista pytań abc</a></li>
+                            <li><a href="pytania-tak-nie">Lista pytań tak/nie</a></li>
+                            <li><a href="dodaj-pytanie-abc">Dodaj pytanie abc</a></li>
+                            <li><a href="dodaj-pytanie-tak-nie">Dodaj pytanie tak/nie</a></li>
+                    </ul>
+            </div>
+    </div>
+</nav>
